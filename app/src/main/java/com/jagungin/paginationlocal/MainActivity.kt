74c23ba4548875de397239de355e4a57f3a70904
Rizky Agung Ramadhan <rizky@rekadia.co.id>
2019-12-16 14:35:06 +0700

    Update MainActivity.kt

--- app/src/main/java/com/jagungin/paginationlocal/MainActivity.kt
+++ app/src/main/java/com/jagungin/paginationlocal/MainActivity.kt
@@ -2,11 +2,135 @@ package com.jagungin.paginationlocal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.jagungin.paginationlocal.adapter.AdapterData
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = javaClass.simpleName
+    lateinit var listData: ArrayList<String>
+    lateinit var listViewType: ArrayList<Int>
+    var countLoadMore by Delegates.notNull<Int>()
+    var isLoading by Delegates.notNull<Boolean>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        isLoading = false
+        listData = ArrayList()
+        listViewType = ArrayList()
+        countLoadMore = 0
+        repeat(30) { a ->
+            listData.add(a.toString())
+            listViewType.add(AdapterData.ITEM_VIEW_TYPE_CONTENT)
+        }
+
+        listData.add("")
+        listViewType.add(AdapterData.ITEM_VIEW_TYPE_LOADING)
+
+        val adapterData = AdapterData(
+            listData = listData,
+            listViewType = listViewType
+        )
+
+        val linearLayoutManager = LinearLayoutManager(this)
+        recycler_view_data_activity_main.layoutManager = linearLayoutManager
+        recycler_view_data_activity_main.adapter = adapterData
+
+
+        recycler_view_data_activity_main.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val countItem = linearLayoutManager.itemCount
+                val lastVisiblePosition = linearLayoutManager.findLastCompletelyVisibleItemPosition()
+                val isLastPosition = countItem.minus(1) == lastVisiblePosition
+                Log.d(TAG, "isLoading: $isLoading & isLastPosition: $isLastPosition & countLoadMore: $countLoadMore")
+                if (!isLoading && isLastPosition && countLoadMore < 3) {
+                    isLoading = true
+                    doAsync {
+                        val lenTemp = listData.size - 1
+                        repeat(10) { a ->
+                            val lastValue: Int = when (a) {
+                                0 -> {
+                                    listData[listData.size - 2].toInt()
+                                }
+                                else -> {
+                                    listData[listData.size - 1].toInt()
+                                }
+                            }
+                            listData.add(lastValue.plus(1).toString())
+                            listViewType.add(AdapterData.ITEM_VIEW_TYPE_CONTENT)
+                        }
+                        Thread.sleep(1000 * 5)
+                        uiThread {
+                            listData.removeAt(lenTemp)
+                            listViewType.removeAt(lenTemp)
+
+                            if (countLoadMore + 1 < 3) {
+                                listData.add("")
+                                listViewType.add(AdapterData.ITEM_VIEW_TYPE_LOADING)
+                            }
+                            adapterData.refresh(listData, listViewType)
+                            countLoadMore += 1
+                            isLoading = false
+                        }
+                    }
+                }
+            }
+        })
+/*        recycler_view_data_activity_main.addOnScrollListener(object :
+            RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val countItem = linearLayoutManager.itemCount
+                val lastVisiblePosition =
+                    linearLayoutManager.findLastCompletelyVisibleItemPosition()
+                val isLastPosition = countItem.minus(1) == lastVisiblePosition
+
+                if (!isLoading && isLastPosition && countLoadMore < 3) {
+                    listData.add("")
+                    listViewType.add(AdapterData.ITEM_VIEW_TYPE_LOADING)
+                    adapterData.refresh(listData, listViewType)
+
+                    isLoading = true
+                    doAsync {
+                        val lenTemp = listData.size - 1
+                        repeat(10) { a ->
+                            val lastValue: Int = when (a) {
+                                0 -> listData[listData.size - 2].toInt()
+                                else -> listData[listData.size - 1].toInt()
+                            }
+                            listData.add(lastValue.plus(1).toString())
+                            listViewType.add(AdapterData.ITEM_VIEW_TYPE_CONTENT)
+                        }
+
+                        Thread.sleep(1000 * 5)
+                        uiThread {
+                            listData.removeAt(lenTemp)
+                            listViewType.removeAt(lenTemp)
+                            adapterData.refresh(listData, listViewType)
+                            countLoadMore += 1
+                            isLoading = false
+                        }
+                    }
+
+
+                }
+
+            }
+
+        })*/
+
+    }
+
+
+    companion object {
+        private const val DELAY = 5000L
     }
 }
